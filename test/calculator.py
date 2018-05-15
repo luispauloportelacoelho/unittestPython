f9728fb4df9cc231ee4f18a50be21d3cde688d17
unknown <luisportelacoelho@gmail.com>
2018-05-16 00:49:36 +0100

    Update syntax in calculator.py

--- test/calculator.py
+++ test/calculator.py
@@ -11,16 +11,16 @@ def mul_values(x, y):
 
 class Tests(TestCase):
     def test_sum(self):
-        self.assertEqual(sum_values(5,5), 10, msg=None)
+        self.assertEqual(sum_values(5, 5), 10, msg=None)
 
     #def test_sum_error(self):
     #    self.assertEquals(sum_values(5,5), 11, msg=None)
 
     def test_sub(self):
-        self.assertEqual(sub_values(5,5), 0, msg=None)
+        self.assertEqual(sub_values(5, 5), 0, msg=None)
 
     def test_mul(self):
-        self.assertEqual(mul_values(5,5), 25, msg=None)
+        self.assertEqual(mul_values(5, 5), 25, msg=None)
 
 
 if __name__ == '__main__':
