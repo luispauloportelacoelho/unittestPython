a4abc8f4d41dfb04a2adcc0128c6ce1dd0b7e26a
unknown <luisportelacoelho@gmail.com>
2018-05-17 22:46:13 +0100

    Refactoring

--- features/calc.feature
+++ features/calc.feature
@@ -1,5 +1,9 @@
 Feature: Sum numbers
 
   Scenario: basic sum
-    When Sum "2" com "2"
+    When Sum "2" with "2"
     Then the result is "4"
+
+  Scenario: sum of float values
+    When Sum "2.0" with "2.0"
+    Then the result is "4.0"
--- features/steps/calcSteps.py
+++ features/steps/calcSteps.py
@@ -2,15 +2,13 @@ from behave import step
 
 from calc import sum_values
 
-result = None
 
 
-@step('Sum "{value_0:d}" with "{value_1:d}"')
+@step('Sum "{value_0}" with "{value_1}"')
 def test_sum(context, value_0, value_1):
-    global result
-    result = sum_values(value_0, value_1)
+    context.result = float(sum_values(float(value_0), float(value_1)))
 
 
-@step('the result is "{result_operation:d}"')
+@step('the result is "{result_operation}"')
 def test_sum_resutl(context, result_operation):
-    assert result == result_operation
+    assert context.result == float(result_operation)
