8c12c607afb5799f93c50789285768f6e5220384
unknown <luisportelacoelho@gmail.com>
2018-05-22 23:11:55 +0100

    Decorator example

---
+++ test3/teste_4.py
@@ -0,0 +1,26 @@
+def decorador(func):
+    def interna(x, y):
+        if isinstance(x, int) and isinstance(y, int):
+            return func(x, y)
+        else:
+            raise ValueError('Insira somente inteiros')
+    return interna
+
+
+"""decorador -> interna -> função decorada"""
+
+
+@decorador
+def sum_values(x: int, y: int):
+    """Sum two values."""
+    return x + y
+
+
+@decorador
+def div_values(x: int, y: int):
+    """Divide two numbers."""
+    return x / y
+
+
+print(sum_values(2, 2))
+print(div_values(3, 'Paulo'))
