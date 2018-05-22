25ce6235a9b4fcf43c522a5fcae9b30915b1e712
unknown <luisportelacoelho@gmail.com>
2018-05-22 23:12:21 +0100

    Decorator used with Flask

---
+++ test3/teste_6.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Ola Live'
+
+
+@app.route('/live')
+def live_page():
+    return 'Estamos online'
+
+app.run()
