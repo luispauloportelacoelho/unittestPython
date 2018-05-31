8ef277c69b46b9473bbf02afb733fc8f4d790482
unknown <luisportelacoelho@gmail.com>
2018-05-31 21:03:17 +0100

    Live 14 - update thread.

--- Live14/02.py
+++ Live14/02.py
@@ -1,23 +1,29 @@
-from threading import Thread
+from threading import Thread, Lock
 c = 0
+lock = Lock()
 
-def count_300():
+def count_30000():
     global c
-    while c < 300:
+    lock.aquire()
+    try:
+        while c < 30000:
+            c += 1
+        print(c)
+    finally:
+        lock.release()
+
+
+def count_100000():
+    global c
+    c = 340
+    while c < 100000:
         c += 1
     print(c)
 
 
-def count_1000():
-    global c
-    c = 350
-    while c != 1000:
-        c += 1
-    print(c)
+t_0 = Thread(target=count_30000, name='30000', daemon=True)
 
+t_1 = Thread(target=count_100000, name='100000', daemon=True)
 
-t_0 = Thread(target=count_300, name='300', daemon=True)
-
-t_1 = Thread(target=count_1000, name='1000', daemon=True)
 t_1.start()
 t_0.start()
